0286dc685bdb449bad1c20116e1114d51cb83e1f
Baig73381 <Baig73381@gmail.com>
2025-05-14 00:50:36 +0530

    Delete task_scheduler directory
    updating folder struecure

--- task_scheduler/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Task Scheduler - A command line tool for scheduling and running tasks in parallel."""
-
-__version__ = "0.1.0"
--- task_scheduler/__main__.py
+++
@@ -1,5 +0,0 @@
-"""Main entry point for the task scheduler."""
-from .cli.cli import main
-
-if __name__ == "__main__":
-    main()
